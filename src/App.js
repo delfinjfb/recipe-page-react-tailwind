4c8a7bd6d6a146261534325e4363a76f28137cf4
Delfin F <DelfinoDevShark@gmail.com>
2024-11-05 21:04:35 +0100

    main element

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import RecipeCard from "./components/RecipeCard";
 
 function App() {
 	return (
-		<>
+		<main>
 			<div className="bg-stone-100 min-h-screen p-4">
 				<RecipeCard />
 			</div>
-		</>
+		</main>
 	);
 }
 
