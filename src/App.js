f059c0c5597cb65cdcc200624e4b55ef1452cbba
Delfin F <DelfinoDevShark@gmail.com>
2024-11-05 21:10:28 +0100

    fix: titles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import RecipeCard from "./components/RecipeCard";
 
 function App() {
+	useEffect(() => {
+		document.title = "Recipe Page:: Simple Omelette Recipe";
+	}, []);
 	return (
 		<main>
 			<div className="bg-stone-100 min-h-screen p-4">
--- src/components/RecipeTitle.jsx
+++ src/components/RecipeTitle.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const RecipeTitle = () => (
 	<div className=" p-4">
-		<h2 className="text-2xl font-serif font-bold text-stone-900">
+		<h1 className="text-2xl font-serif font-bold text-stone-900">
 			Simple Omelette Recipe
-		</h2>
+		</h1>
 		<p className="text-stone-600 mt-2">
 			An easy and quick dish, perfect for any meal. This classic omelette
 			combines beaten eggs cooked to perfection...
