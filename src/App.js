43b3d71fec1ead6eead8dfcd536c39be58eb5593
Delfin F <DelfinoDevShark@gmail.com>
2024-11-05 21:03:36 +0100

    html lang and meta tags

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import RecipeCard from "./components/RecipeCard";
 
 function App() {
 	return (
-		<div className="bg-stone-100 min-h-screen p-4">
-			<RecipeCard />
-		</div>
+		<>
+			<div className="bg-stone-100 min-h-screen p-4">
+				<RecipeCard />
+			</div>
+		</>
 	);
 }
 
