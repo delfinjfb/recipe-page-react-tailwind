3d1e3e7203d31b91ae192492b49a5161bc5399ee
Delfin F <DelfinoDevShark@gmail.com>
2024-11-06 14:16:34 +0100

    Ingredients: font size, margins

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Ingredients = () => (
 	<div className="my-4">
-		<h2 className="text-lg font-serif font-bold text-brown-800">Ingredients</h2>
+		<h2 className="text-3xl  font-serif  text-brown-800">Ingredients</h2>
 		<ul className="list-disc list-inside text-stone-600 mt-2">
-			<li>2-3 large eggs</li>
-			<li>Salt, to taste</li>
-			<li>Pepper, to taste</li>
-			<li>1 tablespoon butter or oil</li>
-			<li>Optional fillings: cheese, vegetables, meats</li>
+			<li className="m-4">2-3 large eggs</li>
+			<li className="m-4">Salt, to taste</li>
+			<li className="m-4">Pepper, to taste</li>
+			<li className="m-4">1 tablespoon butter or oil</li>
+			<li className="m-4">Optional fillings: cheese, vegetables, meats</li>
 		</ul>
 	</div>
 );
