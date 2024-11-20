04fe0bf61379d6847186156d79d49e57f167731c
Delfin F <DelfinoDevShark@gmail.com>
2024-11-20 17:18:20 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import RecipeCard from "./components/RecipeCard";
+import Footer from "./components/Footer";
 
 function App() {
 	useEffect(() => {
@@ -10,6 +11,7 @@ function App() {
 			<div className="bg-stone-100 min-h-screen p-4">
 				<RecipeCard />
 			</div>
+			<Footer />
 		</main>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+// app/components/Footer.tsx
+
+export default function Footer() {
+	return (
+		<footer className="text-center text-sm text-white py-4 bg-black">
+			Challenge by
+			<a
+				href="https://www.frontendmentor.io/?ref=challenge"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-rose-800 hover:underline"
+			>
+				Frontend Mentor
+			</a>
+			. Coded by
+			<a
+				href="https://github.com/delfinjfb"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-rose-800 hover:underline"
+			>
+				Delfin Fernandez
+			</a>
+			.
+		</footer>
+	);
+}
