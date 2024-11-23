0eccd414cafa6df02573ac59048c2b698e0c9fc2
Delfin F <DelfinoDevShark@gmail.com>
2024-11-23 13:09:49 +0100

    RecipeImage: fix

--- src/components/RecipeImage.jsx
+++ src/components/RecipeImage.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const RecipeImage = ({src, alt = "" }) => (
-	<img
-		src={src}
-		alt={alt || ""}
-		className="rounded-t-lg w-full h-85 object-cover"
-	/>
+const RecipeImage = ({src, alt}) => (
+	<img src={src} alt={alt} className="rounded-t-lg w-full h-85 object-cover" />
 );
 
 RecipeImage.propTypes = {
 	src: PropTypes.string.isRequired,
 	alt: PropTypes.string
-};
-
-RecipeImage.defaultProps = {
-	alt: ""
 };
 
 export default RecipeImage;
