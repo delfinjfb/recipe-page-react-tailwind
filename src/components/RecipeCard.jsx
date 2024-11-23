35ae1ac012be398cf2da10547190882ca96fa92d
Delfin F <DelfinoDevShark@gmail.com>
2024-11-23 13:14:50 +0100

    RecipeCard: my-10

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -29,7 +29,7 @@ const RecipeCard = ({handleNextRecipe}) => {
 
 	return (
 		<>
-			<div className="bg-white my-20 shadow-md rounded-lg max-w-lg mx-auto p-9 pb-0 sm:max-w-[375px] md:max-w-[820px]">
+			<div className="bg-white my-10 shadow-md rounded-lg max-w-lg mx-auto p-9 pb-0 sm:max-w-[375px] md:max-w-[820px]">
 				<RecipeImage src={recipe.image} alt={recipe.title} />
 				<div className="p-4">
 					<RecipeTitle title={recipe.title} description={recipe.description} />
