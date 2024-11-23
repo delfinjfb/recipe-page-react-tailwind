dcc711d9ef7c280125ea36ce7895b7fe33f766fd
Delfin F <DelfinoDevShark@gmail.com>
2024-11-23 01:15:17 +0100

    matomo: fix, track only pages

--- src/utils/Matomo.js
+++ src/utils/Matomo.js
@@ -3,21 +3,33 @@ import {useEffect} from "react";
 const Matomo = () => {
 	useEffect(() => {
 		const _paq = (window._paq = window._paq || []);
-		/* Tracker methods like "setCustomDimension" should be called before "trackPageView" */
+
+		// Only track if the current URL is a page, not a resource
+		if (
+			window.location.pathname.endsWith(".png") ||
+			window.location.pathname.endsWith(".jpg")
+		) {
+			console.warn("Matomo: Not tracking resource file as a page view");
+			return;
+		}
+
+		// Track page view and enable link tracking
 		_paq.push(["trackPageView"]);
 		_paq.push(["enableLinkTracking"]);
+		_paq.push(["disableCookies"]);
 
-		(function () {
-			const u = "https://delfinowebsite.matomo.cloud/";
-			_paq.push(["setTrackerUrl", u + "matomo.php"]);
-			_paq.push(["setSiteId", "2"]);
-			const d = document;
-			const g = d.createElement("script");
-			const s = d.getElementsByTagName("script")[0];
-			g.async = true;
-			g.src = "https://cdn.matomo.cloud/delfinowebsite.matomo.cloud/matomo.js";
-			s.parentNode.insertBefore(g, s);
-		})();
+		// Set tracker URL and site ID
+		const u = "https://delfinowebsite.matomo.cloud/";
+		_paq.push(["setTrackerUrl", u + "matomo.php"]);
+		_paq.push(["setSiteId", "2"]);
+
+		// Load Matomo script
+		const d = document;
+		const g = d.createElement("script");
+		const s = d.getElementsByTagName("script")[0];
+		g.async = true;
+		g.src = "https://cdn.matomo.cloud/delfinowebsite.matomo.cloud/matomo.js";
+		s.parentNode.insertBefore(g, s);
 	}, []);
 
 	return null;
