c4d613a5dbd2ca0cae0e6183921636491ea06a48
delfin <delfinjfb@gmail.com>
2025-01-21 03:32:26 +0100

    chore: format code and improve consistency in Footer, Ingredients, and Nutrition components

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Ingredients = ({ingredients}) => (
-	<div className="my-4 border-t-2 pt-4">
-		<h2 className="text-2xl font-serif text-brown-800 mb-2">Ingredients</h2>
-		<ul className="list-disc pl-6">
-			{ingredients.map((ingredient, index) => (
-				<li key={index}>
-					{typeof ingredient.quantity === "number"
-						? `${ingredient.quantity} `
-						: ""}
-					{ingredient.unit && ingredient.unit !== ""
-						? `${ingredient.unit} `
-						: ""}
-					{ingredient.name} {ingredient.note}
-				</li>
-			))}
-		</ul>
-	</div>
+const Ingredients = ({ ingredients }) => (
+  <div className="my-4 border-t-2 pt-4">
+    <h2 className="text-2xl font-serif text-brown-800 mb-2">Ingredients</h2>
+    <ul className="list-disc pl-6">
+      {ingredients.map((ingredient, index) => (
+        <li key={index}>
+          {typeof ingredient.quantity === 'number'
+            ? `${ingredient.quantity} `
+            : ''}
+          {ingredient.unit && ingredient.unit !== ''
+            ? `${ingredient.unit} `
+            : ''}
+          {ingredient.name} {ingredient.note}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 Ingredients.propTypes = {
-	ingredients: PropTypes.arrayOf(
-		PropTypes.shape({
-			quantity: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-			unit: PropTypes.string,
-			name: PropTypes.string.isRequired
-		})
-	).isRequired
+  ingredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      quantity: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      unit: PropTypes.string,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Ingredients;
--- src/components/Nutrition.jsx
+++ src/components/Nutrition.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Nutrition = ({elements}) => (
-	<div className="my-6">
-		<h2>Nutrition</h2>
-		<p className="p-4">
-			The table below shows nutritional values per serving without the
-			additional fillings.
-		</p>
+const Nutrition = ({ elements }) => (
+  <div className="my-6">
+    <h2>Nutrition</h2>
+    <p className="p-4">
+      The table below shows nutritional values per serving without the
+      additional fillings.
+    </p>
 
-		<table className="w-full text-stone-600 border-collapse">
-			<tbody>
-				{elements.map((element, index) => (
-					<tr key={index} className="border-b border-stone-150">
-						<td className="p-4">{element.nutrient}</td>
-						<td className="p-2 text-brown-800 font-bold">
-							{element.quantity} {element.unit}
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	</div>
+    <table className="w-full text-stone-600 border-collapse">
+      <tbody>
+        {elements.map((element, index) => (
+          <tr key={index} className="border-b border-stone-150">
+            <td className="p-4">{element.nutrient}</td>
+            <td className="p-2 text-brown-800 font-bold">
+              {element.quantity} {element.unit}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 Nutrition.propTypes = {
-	elements: PropTypes.arrayOf(
-		PropTypes.shape({
-			nutrient: PropTypes.string.isRequired,
-			quantity: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
-				.isRequired,
-			unit: PropTypes.string.isRequired
-		})
-	).isRequired
+  elements: PropTypes.arrayOf(
+    PropTypes.shape({
+      nutrient: PropTypes.string.isRequired,
+      quantity: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+        .isRequired,
+      unit: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Nutrition;
